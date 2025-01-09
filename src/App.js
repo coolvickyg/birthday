713462f4eb0cc6710ef2591228232077a95f2e30
coolvickyg <coolguy.vickygangil2002@gmail.com>
2025-01-10 02:14:14 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import GreetingCard from './components/GreetingCard';
 import FinalSurprise from './components/FinalSurprise';
